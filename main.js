fbdf03079e7346eddf7d09b7ef8d76d90728a995
Tim Rothämel <hjfhfhfh@gmail.com>
2026-02-04 10:59:27 +0100

    array Karten erweiter Karo Karten (nicht ferig)

---
+++ main.js
@@ -0,0 +1,36 @@
+let karten = [
+    {
+        farbe: "Karo",
+        wert: 2,
+        numWert: 2
+    },
+    {
+        farbe: "Karo",
+        wert: 3,
+        numWert: 3
+    }  ,  {
+        farbe: "Karo",
+        wert: 4,
+        numWert: 4
+    }  ,  {
+        farbe: "Karo",
+        wert: 5,
+        numWert: 5
+    } ,   {
+        farbe: "Karo",
+        wert: 6,
+        numWert: 6
+    }   , {
+        farbe: "Karo",
+        wert: 7,
+        numWert: 7
+    }   , {
+        farbe: "Karo",
+        wert: 8,
+        numWert: 8
+    }   , {
+        farbe: "Karo",
+        wert: 9,
+        numWert: 9
+    }
+]
