5348f134e2ca900a9d40deac5a2c3cd57472d65a
Tim Rothämel <hjfhfhfh@gmail.com>
2026-02-13 10:34:48 +0100

    spielStarten.js erstellt und Aufgaben aufgeschrieben

---
+++ spielStarten.js
@@ -0,0 +1,5 @@
+// Karten Importieren
+// Spieler erstellen
+// Funktion importieren
+// Kartenstapel für die Spieler erstellen -> zufällig Karten ziehen (zwei Spieler)
+// Gemeindschaftskarten (ziehen + Array)
