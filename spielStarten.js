41e73fedbd5692bd06c16d588b8d5ded7cc208c0
Jonah Spiller <fjjfjjfj@gmail.com>
2026-02-13 10:40:01 +0100

    Spieler 1 und 2 erstellt

--- spielStarten.js
+++ spielStarten.js
@@ -1,5 +1,8 @@
 // Karten Importieren
-// Spieler erstellen
 // Funktion importieren
 // Kartenstapel für die Spieler erstellen -> zufällig Karten ziehen (zwei Spieler)
 // Gemeindschaftskarten (ziehen + Array)
+
+//Spieler erstellt
+let spieler1
+let spieler2
