fd23e4edcfa8031b425f5f3ace271c9c8908e24c
Jonah Spiller <fjjfjjfj@gmail.com>
2026-03-04 12:39:17 +0100

    spiel reihenfolge in game-loop.js erstellt

--- game-loop.js
+++ game-loop.js
@@ -1,3 +1,12 @@
 import {spielStarten} from "./spielStarten.js"; // importiert die Funktion spielStarten
 
 spielStarten()
+
+
+// 1. Mindesteinsatz (um spiel zu starten)
+// 2. jeder spieler erhält karten (2)
+// 3. erste einsatz (beginnt bei linken spieler), man kann erhöhen, mitgehen oder aussteigen
+// 4. drei gemeinschaftskarten in mitte, zweite setzrunde beginnt
+// 5. vierte gemeinschaftskarte wird aufgedeckt
+// 6. fünfte gemeinschaftskate wird aufgedeckt, dritte und letze setzrunde
+// 7. wenn nach letzer runde noch mehrere spieler im spiel sind gewinnt beste hand (kriegt das geld)
