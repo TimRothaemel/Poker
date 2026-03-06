5ff8b577e21702a277fdd09890bf355fc5a05d4a
Tim Rothämel <hjfhfhfh@gmail.com>
2026-03-06 12:32:50 +0100

    auf der index.html werden die karten angezeigt

--- spielStarten.js
+++ spielStarten.js
@@ -34,15 +34,15 @@ function kartenZiehen(){
 
 
 function flopsAufdecken(){
-    gemeinschaftsKarten1.innerHTML = gemeinschaftsKarten.flop1[0].farbe
-    gemeinschaftsKarten2.innerHTML = gemeinschaftsKarten.flop2[0].farbe
-    gemeinschaftsKarten3.innerHTML = gemeinschaftsKarten.flop3[0].farbe
+    gemeinschaftsKarten1.innerHTML = `<p class="farbe">${gemeinschaftsKarten.flop1[0].farbe}</p><br><p class="wert">${gemeinschaftsKarten.flop1[0].wert}</p>`;
+    gemeinschaftsKarten2.innerHTML = `<p class="farbe">${gemeinschaftsKarten.flop2[0].farbe}</p><br><p class="wert">${gemeinschaftsKarten.flop2[0].wert}</p>`;
+    gemeinschaftsKarten3.innerHTML = `<p class="farbe">${gemeinschaftsKarten.flop3[0].farbe}</p><br><p class="wert">${gemeinschaftsKarten.flop3[0].wert}</p>`;
 }
 function turnAufdecken(){
-    gemeinschaftsKarten4.innerHTML = gemeinschaftsKarten.turn[0].farbe
+    gemeinschaftsKarten4.innerHTML = `<p class="farbe">${gemeinschaftsKarten.turn[0].farbe}</p><br><p class="wert">${gemeinschaftsKarten.turn[0].wert}</p>`;
 }
 function riverAufdecken(){
-    gemeinschaftsKarten5.innerHTML = gemeinschaftsKarten.river[0].farbe
+    gemeinschaftsKarten5.innerHTML = `<p class="farbe">${gemeinschaftsKarten.river[0].farbe}</p><br><p class="wert">${gemeinschaftsKarten.river[0].wert}</p>`;
 }
 
 export function spielStarten(){
